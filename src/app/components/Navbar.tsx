23f0b3aa814503e7c2fa830b226c974b36df2c8d
ramiro999 <ramiro2191920@correo.uis.edu.co>
2024-02-29 04:19:55 -0500

    general changes

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,75 @@
+"use client";
+import React, { useState } from "react";
+import Link from "next/link";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
+
+// Define an interface for the link items
+interface LinkItem {
+  name: string;
+  url: string;
+}
+
+// Define the links
+export const Links: LinkItem[] = [
+  {
+    name: "Home",
+    url: "/",
+  },
+  {
+    name: "About",
+    url: "/about",
+  },
+  {
+    name: "Projects",
+    url: "/projects",
+  },
+  {
+    name: "Contact",
+    url: "/contact",
+  },
+];
+
+const Navbar: React.FC = () => {
+  const [navbarOpen, setNavbarOpen] = useState(false);
+
+  return (
+    <nav className="fixed mx-auto border border-[#33353F] top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100">
+      <div className="flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2">
+        <Link
+          href={"/"}
+          className="text-2xl md:text-5xl text-white font-semibold"
+        >
+          ra
+        </Link>
+        <div className="mobile-menu block md:hidden">
+          {!navbarOpen ? (
+            <button
+              onClick={() => setNavbarOpen(true)}
+              className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white"
+            >
+              <Bars3Icon className="h-5 w-5" />
+            </button>
+          ) : (
+            <button
+              onClick={() => setNavbarOpen(false)}
+              className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white"
+            >
+              <XMarkIcon className="h-5 w-5" />
+            </button>
+          )}
+        </div>
+        <div className="menu hidden md:block md:w-auto" id="navbar">
+          <ul className="md:flex items-center justify-between text-base text-white pt-4 md:pt-0">
+            {Links.map((link) => (
+              <li key={link.name} className="md:px-4 py-2">
+                <Link href={link.url}>{link.name}</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
