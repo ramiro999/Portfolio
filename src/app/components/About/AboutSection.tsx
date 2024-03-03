0d5f3595222e6e219d884b289f0b3363898680a3
ramiro999 <ramiro2191920@correo.uis.edu.co>
2024-03-03 02:45:37 -0500

    add "About us"

---
+++ src/app/components/About/AboutSection.tsx
@@ -0,0 +1,14 @@
+"use client";
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+
+const AboutSection: React.FC = () => {
+  return (
+    <section className="flex">
+      <h2 className="text-3xl font-bold text-white">About Me</h2>
+    </section>
+  );
+};
+
+export default AboutSection;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Navbar from "./components/Navbar/Navbar";
 import SectionOne from "./components/Sections/SectionOne";
 import SectionTwo from "./components/Sections/SectionTwo";
+import AboutSection from "./components/About/AboutSection";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <div className="container mt-24 mx-auto px-12 py-4">
         <SectionOne />
         <SectionTwo />
+        <AboutSection />
       </div>
     </main>
   );
