da858ace9914a5b7463ed6b870e853fb28b2047c
ramiro999 <ramiro2191920@correo.uis.edu.co>
2024-03-19 22:52:50 -0500

    changes in styles and add images

--- src/app/components/Projects/ProjectsSection.tsx
+++ src/app/components/Projects/ProjectsSection.tsx
@@ -10,12 +10,12 @@ export const Features = [
   {
     title: "ProSoft",
     description: "Platform for project management in a small company",
-    imgP: "/1.png"
+    imgP: "/prosoft.png"
   },
   {
     title: "Todo React",
     description: "App for managing your tasks guided by FrontendMentor",
-    imgP: "/1.png"
+    imgP: "/todo.png"
   },
 ];
 
@@ -26,14 +26,14 @@ const ProjectsSection: React.FC = () => {
           My projects
         </h2>
         {Features.map((feature, index) => (
-            <div key={index} className="da mr-3 relative flex min-h-screen flex-col justify-center overflow-hidden">
+            <div key={index} className="da mr-4 mt-4 relative flex flex-col justify-center">
               <div className="group relative m-0 flex h-72 w-96 rounded-xl shadow-xl ring-gray-900/5 sm:mx-auto sm:max-w-lg">
-                <div className="z-10 h-full w-full overflow-hidden rounded-xl border border-gray-200 opacity-80 transition duration-300 ease-in-out group-hover:opacity-100 dark:border-gray-700 dark:opacity-70">
+                <div className="h-full w-full overflow-hidden rounded-xl border border-gray-200 opacity-80 transition duration-300 ease-in-out group-hover:opacity-100 dark:border-gray-700 dark:opacity-70">
                   <img src={feature.imgP} className="project-img" alt="Project"/>
                 </div>
                 <div className="absolute bottom-0 z-20 m-0 pb-4 ps-4 transition duration-300 ease-in-out group-hover:-translate-y-1 group-hover:translate-x-3 group-hover:scale-110">
 
-                <h2 className= "project-title">{feature.title}</h2>
+                <h2 className= "text-white text-bold">{feature.title}</h2>
                 <p className="project-description">{feature.description}</p>
 
                 </div>
