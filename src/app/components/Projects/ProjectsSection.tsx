658bf7b2fbf0fc321f75b42e3e5c0a34e68fdbf7
ramiro999 <ramiro2191920@correo.uis.edu.co>
2024-03-21 23:54:43 -0500

    wip

--- src/app/components/Projects/ProjectsSection.tsx
+++ src/app/components/Projects/ProjectsSection.tsx
@@ -31,7 +31,7 @@ const ProjectsSection: React.FC = () => {
           My projects
         </h2>
         {Features.map((feature, index) => (
-            <div key={index} className="da mr-4 mt-4 relative flex flex-col justify-center">
+            <div key={index} className="da mr-2 mt-4 relative flex flex-col-1 justify-center">
               <div className="group relative m-0 flex h-72 w-96 rounded-xl shadow-xl ring-gray-900/5 sm:mx-auto sm:max-w-lg">
                 <div className="h-full w-full overflow-hidden rounded-xl border border-gray-200 opacity-80 transition duration-300 ease-in-out group-hover:opacity-100 dark:border-gray-700 dark:opacity-70">
                   <img src={feature.imgP} className="project-img" alt="Project"/>
