29410d3894c5c31a6750566a15f471fb4fec7f47
ramiro999 <ramiro2191920@correo.uis.edu.co>
2024-03-28 17:21:23 -0500

    wip

--- src/app/components/Contact/ContactSection.tsx
+++ src/app/components/Contact/ContactSection.tsx
@@ -1,33 +1,16 @@
-import { useForm, SubmitHandler } from "react-hook-form"
+"use client"
+import React from "react";
 
-type Inputs = {
-  example: string
-  exampleRequired: string
-}
-
-export default function ContactSection() {
-  const {
-    register,
-    handleSubmit,
-    watch,
-    formState: { errors },
-  } = useForm<Inputs>()
-  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data)
-
-  console.log(watch("example")) // watch input value by passing the name of it
-
-  return (
-    /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
-    <form onSubmit={handleSubmit(onSubmit)}>
-      {/* register your input into the hook by invoking the "register" function */}
-      <input defaultValue="test" {...register("example")} />
-
-      {/* include validation with required or other standard HTML validation rules */}
-      <input {...register("exampleRequired", { required: true })} />
-      {/* errors will return when field validation fails  */}
-      {errors.exampleRequired && <span>This field is required</span>}
-
-      <input type="submit" />
+const ContactSection: React.FC = () => {
+  return(
+    <>
+    <div className="container mt-4 mb-2 text-2xl sm:text-4xl text-center font-bold text-whit">Contact</div>
+    <form action="">
+      <label htmlFor=""/>
+      <input/>
     </form>
+    </>
   )
 }
+
+export default ContactSection;
