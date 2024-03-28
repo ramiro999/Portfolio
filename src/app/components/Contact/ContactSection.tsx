b9dd4955d862a84639eb950b11dfbc8e435af1bb
ramiro999 <ramiro2191920@correo.uis.edu.co>
2024-03-28 17:49:45 -0500

    wip

--- src/app/components/Contact/ContactSection.tsx
+++ src/app/components/Contact/ContactSection.tsx
@@ -21,18 +21,25 @@ const ContactSection: React.FC = () => {
   return(
     <>
     <div className="container mt-4 mb-2 text-2xl sm:text-4xl text-center font-bold text-white">Contact</div>
-    <form onSubmit={handleSubmit(onSubmit)}>
-      
-      <label className="container mt-4 mb-2 text-2xl sm:text-4xl text-center font-bold text-white" htmlFor="email">Email</label>
-      <input type="email" id="email" {...register("email")}/>
+    <form className= "max-w-sm mx-auto" onSubmit={handleSubmit(onSubmit)}>
+    
+    <div className="mb-5">
+      <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white" htmlFor="email">Email</label>
+      <input className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" type="email" id="email" {...register("email")}/>
       {errors.email?.message && <p>{errors.email.message}</p>}
+    
+    </div>  
 
-      <label className="container mt-4 mb-2 text-2xl sm:text-4xl text-center font-bold text-white" htmlFor="textarea">Message</label>
-      <input type="textarea" id="textarea" {...register("message")} />
+      <div className="mb-5">
+
+      <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white" htmlFor="textarea">Message</label>
+      <input className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" type="textarea" id="textarea" {...register("message")} />
       {errors.message?.message && <p>{errors.message.message}</p>}
 
+      </div>
 
-      <button className="container mt-4 mb-2 text-2xl sm:text-4xl text-center font-bold text-white"  type="submit">Submit</button>
+
+      <button className="text-white container bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"  type="submit">Submit</button>
 
     </form>
     </>
